e40c5f245c684f4cd0859354fc21996e79c1d9f1
mikeh36 <michael.john.horton@gmail.com>
2020-05-25 13:25:26 -0600

    Add files via upload

---
+++ IsMeteredCheck.ps1
@@ -0,0 +1,24 @@
+﻿<######################################################
+ ScriptPurpose: Check that the computer's NIC is set to 
+    a metered connections
+ Author: Mike Horton
+ Date: 4/6/2020
+######################################################> 
+
+#If ($MeteredAction -eq "MeteredOn") { $MeteredChoice = 2 } else { $MeteredChoice = 0 }
+$DUSMSvcKey = Get-ChildItem -Path "HKLM:\SOFTWARE\Microsoft\DusmSvc\Profiles" -EA SilentlyContinue
+If($DUSMSvcKey -ne $null)
+{
+    Foreach($regObj in $DUSMSvcKey)
+    {
+        $regKey = $regObj.Name
+        $regKey = $regKey -replace "HKEY_LOCAL_MACHINE", "HKLM:"
+        $UserCost = Get-ItemProperty -Path "$($regKey)\*" -Name UserCost -EA SilentlyContinue
+        If($UserCost.UserCost -ne 0)
+        {
+            return $true
+        }
+    
+    }
+}
+return $false
---
+++ IsMeteredRemediation.ps1
@@ -0,0 +1,30 @@
+﻿<######################################################
+ ScriptPurpose: Remediates NICs that are set to be on
+    a metered connection
+ Author: Mike Horton
+ Date: 4/14/2020
+######################################################> 
+
+
+$DUSMSvcKey = Get-ChildItem -Path "HKLM:\SOFTWARE\Microsoft\DusmSvc\Profiles" -EA SilentlyContinue
+If($DUSMSvcKey -ne $null)
+{
+    Remove-Item -Path "HKLM:\SOFTWARE\Microsoft\DusmSvc\Profiles\*" -Recurse -ErrorAction SilentlyContinue
+    #Create Scheduled task to force reboot in the middle of the night
+    if(Get-ScheduledTask -TaskName MeteredNICReboot -ErrorAction SilentlyContinue)
+    {
+        Get-ScheduledTask -TaskName MeteredNICReboot | Unregister-ScheduledTask -Confirm:$False
+    }
+    #Before getting the date make sure that the time isn't currently between 12:00AM and 5:00AM
+    [int]$CurrentHour = (Get-Date).ToString("HH")
+    If($CurrentHour -lt 5)
+    {
+        $Date = (Get-Date).ToString('yyyy/MM/dd')
+    }else{
+        $Date = (Get-Date).AddDays(1).ToString('yyyy/MM/dd')
+    }
+    $Time = "05:01"
+    $action = New-ScheduledTaskAction -Execute C:\Windows\System32\Shutdown.exe -Argument "-r -f -t 60"
+    $trigger = New-ScheduledTaskTrigger -Once -At "$Date.$Time"
+    Register-ScheduledTask -Action $action -Trigger $trigger -TaskName "MeteredNICReboot" -User "SYSTEM" -RunLevel Highest -Force | Out-Null
+}
